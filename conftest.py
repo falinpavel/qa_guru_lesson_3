c5af87e4cf382839e31596a7073fe2ff778226b9
Falin Pavel <falinpavel96@gmail.com>
2025-07-12 14:08:56 +0300

    +1

--- conftest.py
+++ conftest.py
@@ -6,20 +6,25 @@ import pytest
 @allure.title("Open browser and go to Niffler.qa.guru, quit browser after test")
 @pytest.fixture(scope="session", autouse=True)
 def browser_fixture():
-    browser.config.window_width, browser.config.window_height = 1920, 1080
-    browser.open('https://niffler.qa.guru')
+    with allure.step("Open browser and go to Niffler.qa.guru"):
+        browser.config.window_width, browser.config.window_height = 1920, 1080
+        browser.open('https://niffler.qa.guru')
     yield
-    browser.quit()
+    with allure.step("Close browser"):
+        browser.quit()
 
 
 @allure.title("Login in Niffler and logout after test")
 @pytest.fixture(scope="session", autouse=True)
 def login_and_logout_fixture(browser_fixture):
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345')
-    browser.element('button[type="submit"]').click()
+    with allure.step("Login in Niffler"):
+        browser.element('input[name="username"]').type('stas')
+        browser.element('input[name="password"]').type('12345')
+        browser.element('button[type="submit"]').click()
+        browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     yield
-    browser.element('button[aria-haspopup]').should(be.in_dom).click()
-    elements = browser.all('li[role="menuitem"]')
-    elements[3].should(be.visible).click()
-    browser.element('button.css-1v1p78s[type="button"]').click()
+    with allure.step("Logout from Niffler"):
+        browser.element('button[aria-haspopup]').should(be.visible).click()
+        elements = browser.all('li[role="menuitem"]')
+        elements[3].should(be.visible).click()
+        browser.element('button.css-1v1p78s[type="button"]').click()
--- test_niffler_main_menu/test_niffler_search_spending.py
+++ test_niffler_main_menu/test_niffler_search_spending.py
@@ -1,9 +1,7 @@
-import pytest
 from selene import browser, be, have
 import allure
 
 
 @allure.title("Search spending on the main page Niffler")
 def test_search_spending(login_and_logout_fixture):
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     browser.element('input[placeholder="Search"]').type('test123')
