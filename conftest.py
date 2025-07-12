cb62fce5372567e24061f63b45af4f9c674693e8
Falin Pavel <falinpavel96@gmail.com>
2025-07-12 15:40:38 +0300

    refactor and added +1 step for test_niffler_search_spending.py

--- conftest.py
+++ conftest.py
@@ -11,9 +11,6 @@ def setup_browser():
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     browser.config.driver_options = chrome_options
     logging.getLogger("selene").setLevel(logging.ERROR)
     return chrome_options
--- test_niffler_main_menu/test_niffler_search_spending.py
+++ test_niffler_main_menu/test_niffler_search_spending.py
@@ -5,6 +5,8 @@ import allure
 @allure.title("Search spending on the main page Niffler")
 def test_search_spending(login_and_logout):
     with allure.step("Search spending, fill field and press Enter"):
-        browser.element('input[placeholder="Search"]').should(be.visible)
-        browser.element('input[placeholder="Search"]').click().type('test123').press_enter()
+        browser.element('canvas[role="img"]').should(be.visible)
+        browser.element('input[placeholder="Search"]').should(be.visible).click().type('test123').press_enter()
+    with allure.step("Check field is`t empty, and visible text 'There are no spendings'"):
+        browser.element('input[placeholder="Search"]').should(have.value('test123'))
         browser.element('//p[text()]').should(have.text('There are no spendings'))
