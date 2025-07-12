b274c257e46663c6597f9523b8ac2697933b16e8
Falin Pavel <falinpavel96@gmail.com>
2025-07-12 15:45:16 +0300

    make local fixture in test_niffler_main_nemu dir

--- conftest.py
+++ conftest.py
@@ -25,18 +25,3 @@ def open_close_browser(setup_browser):
     yield
     with allure.step("Close browser"):
         browser.quit()
-
-
-@allure.title("Login in Niffler and logout after test")
-@pytest.fixture(scope="session", autouse=True)
-def login_and_logout(open_close_browser):
-    with allure.step("Login in Niffler"):
-        browser.element('input[name="username"]').type('stas')
-        browser.element('input[name="password"]').type('12345')
-        browser.element('button[type="submit"]').click()
-        browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-    yield
-    with allure.step("Logout from Niffler"):
-        browser.element('button[aria-haspopup]').should(be.visible).click()
-        browser.element('//li[@role="menuitem"][contains(text(), "Sign out")]').should(be.visible).click()
-        browser.element('button.css-1v1p78s[type="button"]').click()
